9f342e75807ee3eb9da2ab2022f5a0138449c9d4
Ryan Petschek <petschekr@gmail.com>
2022-11-13 23:57:17 -0500

    Fix condensed nav menu to match expanded appearance

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -33,11 +33,12 @@
 								<template v-for="item in navigation" :key="item.index">
 									<RouterLink v-if="item.name" :to="item.href ?? ''"
 										:class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']">
-										<component :is="item.icon"
+										<component :is="getIcon(item.icon)"
 											:class="[item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300', 'mr-4 flex-shrink-0 h-6 w-6']"
 											aria-hidden="true" />
 										{{ item.name }}
 									</RouterLink>
+									<div v-else style="height: 2px" class="bg-gray-500 my-3"></div>
 								</template>
 							</nav>
 						</div>
