cb70249412f83341c4ba91ff14b7a3729292cf79
Ryan Petschek <petschekr@gmail.com>
2023-10-21 22:39:17 -0400

    Update version to 0.11.0

--- src/views/subviews/Settings.vue
+++ src/views/subviews/Settings.vue
@@ -12,9 +12,10 @@
 			</Switch>
 		</SwitchGroup>
 		<div class="text-center text-lg mt-2">
-			<code>Flight Director v0.10.1 @ {{commitHash}}</code>
+			<code>Flight Director v0.11.0 @ {{commitHash}}</code>
 		</div>
 		<div class="mt-2">
+			<p><code>v0.11.0</code>: Cavok integration pulls live aircraft data for the performance calculators automatically, implement best rate of climb calculation</p>
 			<p><code>v0.10.0</code>: Add best range calculator to the Performance tab, implement METAR/TAF pull from AF weather</p>
 			<p><code>v0.9.0</code>: Cards that display Markdown content, cancel SharePoint loading process to immediately open cached copy</p>
 			<p><code>v0.8.0</code>: SharePoint files now supported, cards can be moved to another tab via drag and drop, minor bugfixes</p>
