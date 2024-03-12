27a878e3cfbdb9584343af03f420e18ad5e3bb0e
Ryan Petschek <petschekr@gmail.com>
2024-03-13 00:58:28 +0300

    Fix short callsign generation + when dropdown appears

--- src/views/subviews/FileTab.vue
+++ src/views/subviews/FileTab.vue
@@ -6,7 +6,7 @@
 				<slot />
 			</div>
 			<div class="flex flex-1 justify-start mt-2 sm:justify-end sm:mt-0">
-				<div class="w-full sm:w-40">
+				<div v-if="callsigns.length > 0 || editMode" class="w-full sm:w-40">
 					<label for="callsign" class="block text-sm font-medium text-gray-700">Callsign:</label>
 					<select id="callsign" :value="selectedCallsign" @change="callsignChange"
 						class="mt-1 block w-full rounded-md font-bold border-gray-300 py-2 pl-3 pr-10 text-base focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm">
@@ -139,7 +139,7 @@ function callsignChange(event: Event) {
 
 function generateShortCallsign(callsign: string): string {
 	let shortCallsign = callsign;
-	let callsignComponents = callsign.match(/^([a-zA-Z]+)(\d.*)$/);
+	let callsignComponents = callsign.match(/^([a-zA-Z]+) *(\d.*)$/);
 	if (callsignComponents) {
 		shortCallsign = (callsignComponents[1][0] + callsignComponents[1][callsignComponents[1].length - 1]).toUpperCase() + callsignComponents[2]; // Grabs first + last letter + numbers of callsign
 	}
